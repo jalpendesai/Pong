d90a0dbfd6bbcd9b3f6ade2a2cdef3bfff01fdcf
jalpendesai <djalpen24@gmail.com>
2017-10-29 23:16:43 -0700

    Added Paddles & Ball movement

---
+++ Pong_Homework/Assets/Scripts/PaddleLeft.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleLeft : MonoBehaviour {
+
+    public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0f,Input.GetAxis("Vertical1") * speed * Time.deltaTime, 0f);
+	}
+}
