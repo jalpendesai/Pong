006e386f238bf2254131b32568f2be6a029c3ea8
jalpendesai <djalpen24@gmail.com>
2017-10-30 23:17:22 -0700

    added ball spawning and few other minor changes

--- Pong_Homework/Assets/Scripts/Ball.cs
+++ Pong_Homework/Assets/Scripts/Ball.cs
@@ -7,14 +7,35 @@ public class Ball : MonoBehaviour {
     public float speed = 5f; 
 	// Use this for initialization
 	void Start () {
+        LaunchBall();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < -10f)
+        {
+            StartCoroutine(Pause());
+        }
+
+        if (transform.position.x > 10f)
+        {
+            StartCoroutine(Pause());
+        }
+    }
+
+    void LaunchBall()
+    {
+        transform.position = Vector2.zero;
         float sx = Random.Range(0, 2) == 0 ? -1 : 1;
         float sy = Random.Range(0, 2) == 0 ? -1 : 1;
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(speed * sx, speed * sy);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(2.5f);
+        LaunchBall();
+    }
 }
